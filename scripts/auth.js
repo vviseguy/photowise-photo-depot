63c5855e13c6bd2c63e37574c0d914ed27fa8a94
Jacob Wise <jacob@wisefam.net>
2024-12-30 10:55:33 -0700

    test

--- scripts/auth.js
+++ scripts/auth.js
@@ -3,25 +3,14 @@ let userAuthenticated = false;
 let authCode = null;
 
 function redirectToLogin() {
-<<<<<<< Updated upstream
-  if (window.self !== window.top) {
-    // Notify parent page to handle login redirect
-    window.parent.postMessage({ type: 'redirect_to_login', embedPageUrl: window.location.href }, '*');
-    return;
-  }
 
-  const currentUrl = window.location.href.split('?')[0];
-  const loginUrl = `https://us-west-2b2hpjjqgl.auth.us-west-2.amazoncognito.com/login/continue?client_id=59e3vejubvjscpv0vlkkrp1orq&redirect_uri=${encodeURIComponent(
-    currentUrl
-  )}&response_type=code&scope=email+openid+phone`;
-=======
   const currentUrl = window.location.href.split("?")[0];
 
   const loginUrl = `https://us-west-2b2hpjjqgl.auth.us-west-2.amazoncognito.com/login/continue?client_id=59e3vejubvjscpv0vlkkrp1orq&redirect_uri=${encodeURIComponent(
     currentUrl
   )}&response_type=code&scope=email+openid+phone`;
   console.log(loginUrl);
->>>>>>> Stashed changes
+
   window.location.href = loginUrl;
 }
 
@@ -32,14 +21,6 @@ function handleAuthRedirect() {
     console.log('User authenticated with code:', authCode);
     userAuthenticated = true;
     removeCodeFromUrl();
-<<<<<<< Updated upstream
-  }
-
-  if (window.self !== window.top) {
-    // Notify parent page of the authentication code
-    window.parent.postMessage({ type: 'auth_code', code: authCode }, '*');
-=======
->>>>>>> Stashed changes
   }
 }
 
